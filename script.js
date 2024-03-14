30b02ccbc1bd91fa081a458eb9f02ffb93774e77
sherwanm <mahmood@webcellent.com>
2024-03-14 20:23:44 +0100

    File selector from sidbar

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const listSection = document.querySelector('.list-section')
 const listContainer = document.querySelector('.list')
 const fileSelector = document.querySelector('.file-selector')
 const fileSelectorInput = document.querySelector('.file-selector-input')
+const fileSelectorFromSidebar = document.querySelector('.upload-from-sidebar');
 
 // upload files with browse button
-fileSelector.onclick = () => fileSelectorInput.click()
+fileSelector.onclick = () => fileSelectorInput.click();
+
 fileSelectorInput.onchange = () => {
     [...fileSelectorInput.files].forEach((file) => {
         if(typeValidation(file.type)){
@@ -13,6 +15,10 @@ fileSelectorInput.onchange = () => {
         }
     })
 }
+
+// upload files with upload button form sidebar
+fileSelectorFromSidebar.onclick = () => fileSelectorInput.click();
+
 
 // when file is over the drag area
 dropArea.ondragover = (e) => {
